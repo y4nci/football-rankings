808568267f0f138297aef70aca94af656e703015
y4nci <baran.yanci@metu.edu.tr>
2023-03-13 17:56:47 +0300

    edit header

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,9 @@ const App = () => {
                             && (<div className="content">
                                 <Switch>
                                     <Route exact path="/mackolik" component={Home} />
-                                    <Route path="/leagues" component={LeagueGrid} />
+                                    <Route path="/mackolik/leagues">
+                                        <h1 style={{ fontSize:'xxx-large', marginTop: '20px' }}>All Leagues</h1>
+                                    </Route>
                                     {leagues.map((league: League, index) => (
                                         <Route exact path={`/mackolik/${league.slug}/:id`} key={index}>
                                             <StandingsPage leagueName={league.slug}/>
--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -13,16 +13,15 @@ const Header = () => {
             </h1>
             <div className="links">
                 {/*https://a.espncdn.com/i/leaguelogos/soccer/500-dark/11.png*/}
-                <Link to={`/mackolik/eng.1/${currentSeason}`}>
-                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/23.png" alt="img" /></Link>
-                <Link to={`/mackolik/ned.1/${currentSeason}`}>
-                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/11.png" alt="img" /></Link>
-                <Link to={`/mackolik/fra.1/${currentSeason}`}>
-                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/9.png" alt="img" /></Link>
-                <Link to={`/mackolik/ger.1/${currentSeason}`}>
-                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/10.png" alt="img" /></Link>
-                <Link to={`/mackolik/ita.1/${currentSeason}`}>
-                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/12.png" alt="img" /></Link>
+                <Link to={`/mackolik/uefa.champions/${currentSeason}`}>
+                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/2.png" alt="img" /></Link>
+                <Link to={`/mackolik/uefa.europa/${currentSeason}`}>
+                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/2310.png" alt="img" /></Link>
+                <Link to={`/mackolik/uefa.europa.conf/${currentSeason}`}>
+                    <img className="navbar-logo" src="https://a.espncdn.com/i/leaguelogos/soccer/500/20296.png" alt="img" /></Link>
+                <hr style={{ marginRight: '32px', marginLeft: '16px', height: '64px' }}/>
+                <Link to={'/mackolik/leagues'}>
+                    <h3>All Leagues</h3></Link>
             </div>
         </nav>
     );
