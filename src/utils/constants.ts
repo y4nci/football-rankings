bb3161baea413165435c93bf06a372175f1d07f8
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-04-29 17:01:12 +0300

    solve misplaced team stats

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,4 +7,4 @@ export const PALETTE = ['#efddcd', '#ff5f65', '#0cc2bc', '#e28d00', '#c2e12e'];
 
 export const TEAM_STATS = [ 'GP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'P' ];
 
-export const TEAM_STATS_INDEX_MAP = [ 0, 6, 5, 1, 4, 3, 8, 2 ];
+export const TEAM_STATS_INDEX_MAP = [ 0, 7, 6, 1, 5, 4, 2, 3 ];
